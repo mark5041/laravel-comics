1b479f17623d4a79fa286b555f5f5f9884de6043
mark5041 <mark.beloso5041@gmail.com>
2022-02-11 16:50:15 +0100

    header

---
+++ resources/views/guest/partials/header.blade.php
@@ -0,0 +1,19 @@
+<div class="nav">
+        <div class="container">
+            <div class="logo">
+                <img src="{{asset('img/dc-logo.png')}}" alt="">
+            </div>
+            <ul>
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
+            </ul>
+        </div>
+    </div>
